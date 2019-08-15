32e83931ea9b0c70bdb9b1dd0276cf1bb8c55a6f
Alexander <al.deimosfrontend@gmail.com>
2019-08-15 23:05:35 +0300

    final fixes

--- source/js/global.js
+++ source/js/global.js
@@ -1,6 +1,6 @@
 var mainNav = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".page-header__toggle");
-var headerBackground = document.querySelector(".page-header--background");
+var headerBackground = document.querySelector(".page-header__background");
 var headerLogoWhite = document.querySelector(".page-header__picture--white");
 var headerLogoBlue = document.querySelector(".page-header__picture--blue");
 
@@ -15,7 +15,7 @@ navToggle.addEventListener("click", function () {
     navToggle.classList.add("page-header__toggle--opened");
     mainNav.classList.remove("main-nav__toggle-closed");
     mainNav.classList.add("main-nav__toggle-opened");
-    headerBackground.classList.add("page-header--background-white");
+    headerBackground.classList.add("page-header__background--white");
     headerLogoWhite.classList.remove("page-header__picture--opener");
     headerLogoBlue.classList.add("page-header__picture--opener");
   } else {
@@ -23,7 +23,7 @@ navToggle.addEventListener("click", function () {
     navToggle.classList.add("page-header__toggle--closed");
     mainNav.classList.remove("main-nav__toggle-opened");
     mainNav.classList.add("main-nav__toggle-closed");
-    headerBackground.classList.remove("page-header--background-white");
+    headerBackground.classList.remove("page-header__background--white");
     headerLogoWhite.classList.add("page-header__picture--opener");
     headerLogoBlue.classList.remove("page-header__picture--opener");
   }
